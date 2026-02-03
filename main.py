1e19ddf8ab84280a30c031bc8822794b1f8353ac
silakozik <silakozik2005@gmail.com>
2026-02-03 20:35:40 +0300

    implement FastAPI application with CRUD endpoints for text posts and update main entry point to run the app using uvicorn.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def main():
-    print("Hello from socialmediatrial!")
+import  uvicorn
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
