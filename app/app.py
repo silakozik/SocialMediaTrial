f3480846328831673ef805b9aa11191dce1645c0
silakozik <silakozik2005@gmail.com>
2026-02-05 18:21:36 +0300

    retrieving from database

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException, File, UploadFile, Depends
+from fastapi import FastAPI, HTTPException, File, UploadFile, Depends, Form
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, get_async_session, create_db_and_tables
 from sqlalchemy.ext.asyncio import AsyncSession 
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 
 @asynccontextmanager
@@ -29,3 +30,25 @@ async def upload_file(
     await session.commit()
     await session.refresh(post)
     return post
+
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+): 
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
+
+    return {"posts": posts_data}
