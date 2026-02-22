3c32e3bc05e9b203bc5e855af137a5a7597287e0
silakozik <silakozik2005@gmail.com>
2026-02-22 23:07:14 +0300

    added: post deletion

--- app/app.py
+++ app/app.py
@@ -86,3 +86,21 @@ async def get_feed(
         )
 
     return {"posts": posts_data}
+
+    @app.delete(f"/posts({post_id})")
+    async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session)):
+        try:
+            post_uuid = uuid.UUID(post_id)
+
+            result = await session.execute(select(Post).where(Post.id == post_uuid))
+            post =result.scalars().first()
+
+            if not post:
+                raise HTTPException(status_code=404, detail="Post not found")
+
+                await session.delete(post)
+                await session.commit()
+
+                return {"success": True, "message": "Post deleted successfully"}
+        except Exception as e:
+                raise HTTPException(status_code=500, detail=str(e))
