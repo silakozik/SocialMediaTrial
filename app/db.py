407bbf6e4a8ddbc6a50174af18cf350efb78c2c6
silakozik <silakozik2005@gmail.com>
2026-02-25 14:53:50 +0300

    refactor: update database schema to use GUID for IDs and improve user-post relationship

--- app/db.py
+++ app/db.py
@@ -1,14 +1,14 @@
 from collections.abc import AsyncGenerator
-from telnetlib import AYT
 import uuid
 
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 from datetime import datetime
-from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
+
 from fastapi import Depends
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
+from fastapi_users_db_sqlalchemy.generics import GUID
 
 DATABASE_URL = "sqlite+aiosqlite:///./socialmedia.db"
 
@@ -17,13 +17,14 @@ class Base(DeclarativeBase):
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
-    posts =relationship("Post", back_populates="user")
+    posts = relationship("Post", back_populates="user")
+
 
 class Post(Base):
     __tablename__ = "posts"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    id = Column(GUID, primary_key=True, default=uuid.uuid4)
+    user_id = Column(GUID, ForeignKey("user.id"), nullable=False)
     caption = Column(Text)
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False) 
